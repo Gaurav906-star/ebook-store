cac252f8c6e1b6f01e351ef5f01556812dedf474
Study <studyisro24@gmail.com>
2025-12-03 16:21:48 +0000

    added db

--- ebook/settings.py
+++ ebook/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+import pymysql
+pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,8 +79,15 @@ WSGI_APPLICATION = 'ebook.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ebookdb',
+        'USER': 'ebookuser',
+        'PASSWORD': 'test@123',
+        'HOST': '13.218.119.199',
+        'PORT': '3306',
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+        }
     }
 }
 
